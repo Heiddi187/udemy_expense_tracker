99e90a06ef9359fd1ab04b8d5af8227509c39897
Heiddi187 <heiddi187@gmail.com>
2025-05-06 14:03:43 +0000

    darkmode activated

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,30 @@ import 'package:flutter/material.dart';
 import 'widgets/expenses.dart';
 
 var kColorScheme = ColorScheme.fromSeed(
-  seedColor: Color.fromARGB(255, 43, 13, 113),
+  seedColor: Color.fromARGB(255, 148, 131, 189),
+);
+
+var kDarkColorScheme = ColorScheme.fromSeed(
+  brightness: Brightness.dark,
+  seedColor: Color.fromARGB(255, 5, 99, 125),
 );
 
 void main() {
   runApp(
     MaterialApp(
+      darkTheme: ThemeData.dark().copyWith(
+        colorScheme: kDarkColorScheme,
+        cardTheme: CardTheme().copyWith(
+          color: kDarkColorScheme.secondaryContainer,
+          margin: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kDarkColorScheme.primaryContainer,
+            foregroundColor: kDarkColorScheme.onPrimaryContainer,
+          ),
+        ),
+      ),
       theme: ThemeData().copyWith(
         //scaffoldBackgroundColor: const Color.fromARGB(255, 239, 163, 232),
         //appBarTheme: AppBarTheme(
@@ -35,6 +53,7 @@ void main() {
           ),
         ),
       ),
+      //themeMode: ThemeMode.system,
       debugShowCheckedModeBanner: false,
       home: Expenses(),
     ),
