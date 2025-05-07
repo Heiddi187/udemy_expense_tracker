f2009a0861ee36e9accc8f9549f52a92bedbad8c
Heiddi187 <heiddi187@gmail.com>
2025-05-07 11:17:08 +0000

    reversed some changes to get the app running again

--- lib/main.dart
+++ lib/main.dart
@@ -12,55 +12,51 @@ var kDarkColorScheme = ColorScheme.fromSeed(
 );
 
 void main() {
-  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]).then((
-    fn,
-  ) {
-    runApp(
-      MaterialApp(
-        darkTheme: ThemeData.dark().copyWith(
-          colorScheme: kDarkColorScheme,
-          cardTheme: CardTheme().copyWith(
-            color: kDarkColorScheme.secondaryContainer,
-            margin: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-          ),
-          elevatedButtonTheme: ElevatedButtonThemeData(
-            style: ElevatedButton.styleFrom(
-              backgroundColor: kDarkColorScheme.primaryContainer,
-              foregroundColor: kDarkColorScheme.onPrimaryContainer,
-            ),
+  runApp(
+    MaterialApp(
+      darkTheme: ThemeData.dark().copyWith(
+        colorScheme: kDarkColorScheme,
+        cardTheme: CardTheme().copyWith(
+          color: kDarkColorScheme.secondaryContainer,
+          margin: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kDarkColorScheme.primaryContainer,
+            foregroundColor: kDarkColorScheme.onPrimaryContainer,
           ),
         ),
-        theme: ThemeData().copyWith(
-          //scaffoldBackgroundColor: const Color.fromARGB(255, 239, 163, 232),
-          //appBarTheme: AppBarTheme(
-          //backgroundColor: const Color.fromARGB(255, 230, 82, 230),
-          //),
-          colorScheme: kColorScheme,
-          appBarTheme: AppBarTheme().copyWith(
-            backgroundColor: kColorScheme.onPrimaryContainer,
-            foregroundColor: kColorScheme.primaryContainer,
-          ),
-          cardTheme: CardTheme().copyWith(
-            color: kColorScheme.secondaryContainer,
-            margin: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-          ),
-          elevatedButtonTheme: ElevatedButtonThemeData(
-            style: ElevatedButton.styleFrom(
-              backgroundColor: kColorScheme.primaryContainer,
-            ),
-          ),
-          textTheme: ThemeData().textTheme.copyWith(
-            titleLarge: TextStyle(
-              fontWeight: FontWeight.bold,
-              color: kColorScheme.onSecondaryContainer,
-              fontSize: 16,
-            ),
-          ),
-        ),
-        //themeMode: ThemeMode.system,
-        debugShowCheckedModeBanner: false,
-        home: Expenses(),
       ),
-    );
-  }); // app locked to one orientation
+      theme: ThemeData().copyWith(
+        //scaffoldBackgroundColor: const Color.fromARGB(255, 239, 163, 232),
+        //appBarTheme: AppBarTheme(
+        //backgroundColor: const Color.fromARGB(255, 230, 82, 230),
+        //),
+        colorScheme: kColorScheme,
+        appBarTheme: AppBarTheme().copyWith(
+          backgroundColor: kColorScheme.onPrimaryContainer,
+          foregroundColor: kColorScheme.primaryContainer,
+        ),
+        cardTheme: CardTheme().copyWith(
+          color: kColorScheme.secondaryContainer,
+          margin: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kColorScheme.primaryContainer,
+          ),
+        ),
+        textTheme: ThemeData().textTheme.copyWith(
+          titleLarge: TextStyle(
+            fontWeight: FontWeight.bold,
+            color: kColorScheme.onSecondaryContainer,
+            fontSize: 16,
+          ),
+        ),
+      ),
+      //themeMode: ThemeMode.system,
+      debugShowCheckedModeBanner: false,
+      home: Expenses(),
+    ),
+  ); // app locked to one orientation
 }
